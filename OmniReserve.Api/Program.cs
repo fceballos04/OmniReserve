1dd405050e0a837962c03cfb273ccb9a72b401ee
fceballos04 <fceballos.26376@umsa.edu.mx>
2026-09-15 13:38:48 -0600

    Con la clave que subio el maestro aprendí a como Agregar el servicio generador de Swagger (DI) y Agregar el Middleware en el Pipeline HTTP

--- OmniReserve.Api/Program.cs
+++ OmniReserve.Api/Program.cs
@@ -1,23 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// 1. Agregar el servicio generador de Swagger (DI)
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 2. Agregar el Middleware en el Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+// 3. Middlewares restantes
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
