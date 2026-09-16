c86f6be8f34eec364d0f8949dc76cdf1b3f3e6ba
Fernando Ceballos Palma <fceballos.26376@umsa.edu.mx>
2026-09-15 20:23:16 -0600

    Se creó enums
    Se crearon los enums y la entidad user

---
+++ OmniReserve.Domain/Entities/Room.cs
@@ -0,0 +1,6 @@
+﻿namespace OmniReserve.Domain;
+
+public enum Room
+{
+
+}
---
+++ OmniReserve.Domain/Enums/Enums.cs
@@ -0,0 +1,5 @@
+﻿namespace OmniReserve.Domain.Enums;
+
+public enum RoomType { Single, Double, Suite }
+public enum ReservationStatus { Pending, Confirmed, Cancelled, Completed }
+public enum Role { Customer, Admin }
