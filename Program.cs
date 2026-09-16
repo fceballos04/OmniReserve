52f0b37f0693b00a311f937776030d8dc9c04fe3
Fernando Ceballos Palma <fceballos.26376@umsa.edu.mx>
2026-09-15 20:13:55 -0600

    Lo que aprendí
    Con la clave del maestro aprendí que debo crear los builder.services en la Api y eso fue lo que se hizo en el program.cs de la Api y ahí mismo inyectar las dependencias

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using OmniReserve.Application;
+using OmniReserve.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Llama a los métodos de extensión para inyectar las dependencias de otras capas
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
