47ef0284e4cb0cd3293786b1715a0104867f1603
Fernando Ceballos Palma <fceballos.26376@umsa.edu.mx>
2026-09-15 23:54:46 -0600

    Se implementa
    Se implementa el mediatR

---
+++ OmniReserve.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection; // Necesario para Assembly
+
+namespace OmniReserve.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Se registra MediatR
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
